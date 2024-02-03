ccf511121ea93dcbb6e5e383b186351298a7d846
Oscare101 <kirillfedortsev2000@gmail.com>
2024-02-03 12:07:57 +0200

    clear imports

--- CalendarPicker/Calendar.tsx
+++ CalendarPicker/Calendar.tsx
@@ -1,29 +1,11 @@
 import {useState} from 'react';
-import {
-  Dimensions,
-  FlatList,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import {
-  GetCurrentMonthIndex,
-  GetCurrentYear,
-  GetDaysTable,
-} from './functions/functions';
-import GradientText from './components/GradientText';
-import weekDaysShort from './constants/weekDaysShort';
-import montsNames from './constants/montsNames';
-import colors from './constants/colors';
+import {View} from 'react-native';
+import {GetCurrentMonthIndex, GetCurrentYear} from './functions/functions';
 import {CalendarProps} from './constants/interfaces';
 import styles from './constants/styles';
-import {RenderWeekDayItem} from './components/RenderWeekDayItem';
-import {RenderDayItem} from './components/RenderDateItem';
 import MonthsBlock from './components/MonthsBlock';
 import WeekDaysTable from './components/WeekDaysTable';
 import DatesTable from './components/DatesTable';
-const {width, height} = Dimensions.get('screen');
 
 export default function Calendar(props: CalendarProps) {
   const [year, setYear] = useState<number>(GetCurrentYear());
